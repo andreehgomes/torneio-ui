5971aab158a35d7eef7e440071b4e6eed9b8d272
andreehgomes <andrefelipefeliciogomes@gmail.com>
2021-01-25 09:44:08 -0300

    Ajuste no pipe cpfcnpj

--- src/app/_pipes/cpfcnpj.pipe.ts
+++ src/app/_pipes/cpfcnpj.pipe.ts
@@ -5,13 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CpfcnpjPipe implements PipeTransform {
 
-  transform(value: string, ...args: any[]): any {
+  transform(value: string): string {
     if (value.length === 11) {
       return value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '\$1.\$2.\$3\-\$4');
     }
     if (value.length === 14) {
       return value.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/g, '\$1.\$2.\$3\/\$4\-\$5');
     }
-    return 'error';
+    return value;
   }
 }
